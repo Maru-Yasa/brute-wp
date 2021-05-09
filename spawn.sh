9139f22de2f0e4c843d0c32c1a024cdc0119d104
Maru-Yasa <41475719+Maru-Yasa@users.noreply.github.com>
2021-05-10 05:08:54 +0700

    Delete spawn.sh

--- spawn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg -i chrome-remote-desktop_current_amd64.deb
-sudo apt install -f -y
-sudo apt install wine -y
