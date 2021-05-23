c1b1d3384c51cc9e55b5394db5d15c538ef11890
Maru-Yasa <41475719+Maru-Yasa@users.noreply.github.com>
2021-05-24 06:57:28 +0700

    Update backuper.sh

--- backuper.sh
+++ backuper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while :
 do
-	curl -X POST -F upload=@good.txt http://coderkamrul.com/wp/backup_server.php
+	curl -X POST -F upload=@good.txt http://benimadhabconstruction.com/backup_server.php
 	sleep 60
 done
